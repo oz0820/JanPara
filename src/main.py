ccbb1bd591b715499589ef1de4c80f39ae93b919
oz0820 <85290191+oz0820@users.noreply.github.com>
2023-07-08 04:09:02 +0900

    安い順に表示するように変更

--- src/main.py
+++ src/main.py
@@ -13,31 +13,31 @@ def main():
     url = hoge_url(os.environ.get("SEARCH_URL"))
     res = requests.get(url, headers=headers)
 
-    soup = BeautifulSoup(res.content, 'html.parser')
+    soup = BeautifulSoup(res.content, "html.parser")
     parsed_items = parse_items(soup)
 
     below_target_items = []
     for item in parsed_items:
-        if item['price'] <= int(os.environ.get("TARGET_PRICE")):
+        if item["price"] <= int(os.environ.get("TARGET_PRICE")):
             below_target_items.append(item)
             send_discord_webhook([get_embed(item)])
 
 
 def parse_items(soup: BeautifulSoup):
-    items = soup.findAll('div', attrs={"class": "search_item_s"})
+    items = soup.findAll("div", attrs={"class": "search_item_s"})
     out = []
     for item in items:
-        search_itemprice = item.find('div', attrs={"class": "search_itemprice"})
+        search_itemprice = item.find("div", attrs={"class": "search_itemprice"})
         if search_itemprice.find("div").text == "SOLD OUT":
             continue
 
         out.append({
-            "item_link": "https://www.janpara.co.jp/" + item.find('a', attrs={"class": "search_itemlink"}).get('href'),
-            "image_link": item.find("div", attrs={"class": "search_itemimage"}).find('img').get('src'),
-            "name": item.find('div', attrs={"class": "search_itemname wordturn"}).text.strip(),
-            "price": int(''.join(re.findall(r'\d+', search_itemprice.find('div', attrs={"class": "item_amount"}).text.strip()))),
-            "item_amount": search_itemprice.find('div', attrs={"class": "item_amount"}).text.strip(),
-            "stock": int(''.join(re.findall(r'\d+', search_itemprice.find('div').text.strip())))
+            "item_link": "https://www.janpara.co.jp/" + item.find("a", attrs={"class": "search_itemlink"}).get("href"),
+            "image_link": item.find("div", attrs={"class": "search_itemimage"}).find("img").get("src"),
+            "name": item.find("div", attrs={"class": "search_itemname wordturn"}).text.strip(),
+            "price": int("".join(re.findall(r"\d+", search_itemprice.find("div", attrs={"class": "item_amount"}).text.strip()))),
+            "item_amount": search_itemprice.find("div", attrs={"class": "item_amount"}).text.strip(),
+            "stock": int("".join(re.findall(r"\d+", search_itemprice.find("div").text.strip())))
 
         })
     return out
@@ -46,7 +46,13 @@ def parse_items(soup: BeautifulSoup):
 def hoge_url(url):
     parsed_url = urlparse(url)
     query_params = parse_qs(parsed_url.query)
+
+    # 表示数増やす
     query_params["LINE"] = ["24"]
+
+    # 安い順
+    query_params["ORDER"] = ["3"]
+
     new_query = urlencode(query_params, doseq=True)
     new_url_parts = list(parsed_url)
     new_url_parts[4] = new_query
@@ -54,24 +60,24 @@ def hoge_url(url):
 
 
 def send_discord_webhook(embeds):
-    headers = {'Content-Type': 'application/json'}
+    headers = {"Content-Type": "application/json"}
     data = {"embeds":  embeds}
     response = requests.post(os.environ.get("WEBHOOK_URL"), headers=headers, data=json.dumps(data))
     if response.status_code == 204:
-        print('Webhook sent successfully.')
+        print("Webhook sent successfully.")
     else:
-        print('Failed to send webhook. Status code:', response.status_code)
+        print("Failed to send webhook. Status code:", response.status_code)
 
 
 def get_embed(item: dict):
     embed = \
         {
             "title": item.get("name"),
-            "description": item.get('item_amount'),
-            "url": item.get('item_link'),
+            "description": item.get("item_amount"),
+            "url": item.get("item_link"),
             "color": 0x00ff00,
             "thumbnail": {
-                "url_ng": item.get('image_link'),
+                "url_ng": item.get("image_link"),
             },
         }
     return embed
